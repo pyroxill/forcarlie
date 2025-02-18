37d53cc16ecea9a311653011a795498441e41890
Jayden Robertson <jaydensouterrobertson@gmail.com>
2025-02-18 10:01:05 +0000

    Update MySongToYou.tsx

--- src/pages/MySongToYou.tsx
+++ src/pages/MySongToYou.tsx
@@ -80,16 +80,7 @@ And in a moment, I knew you best`}
                 </CardTitle>
               </CardHeader>
               <CardContent>
-                <iframe 
-                  style={{ borderRadius: "12px" }} 
-                  src="https://open.spotify.com/embed/playlist/6c9qC0v6b1PEjiqlSiIxTm?utm_source=generator&theme=0" 
-                  width="100%" 
-                  height="152" 
-                  frameBorder="0" 
-                  allowFullScreen 
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                  loading="lazy"
-                />
+               <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0gkaMm4dsDgMowjCOpbA8l?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               </CardContent>
             </div>
           </CardContent>
